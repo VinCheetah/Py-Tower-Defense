c760f2dc94024fa424bf771a91a275817ed081c5
GARDIES <vincent.gardies@ens-lyon.fr>
2023-10-16 14:29:35 +0200

    CLEAN : animation (part 1)

--- animationClass.py
+++ animationClass.py
@@ -9,6 +9,24 @@ from math import pi, cos, sin
 
 
 class Animation:
+
+    def from_origin(self, origin, config):
+        Animation.__init__(self, origin.game, config, origin.x, origin.y)
+        self.origin = origin
+
+    def __init__(self, game, config, x, y):
+        self.game = game
+        self.config = self.game.config.animation.basics | config
+
+        self.type = self.config.type
+        self.original_life_time = self.config.life_time
+        self.life_time = self.original_life_time
+        self.color = self.config.color
+        self.screen = self.game.map_window.window
+        self.alpha_screen = self.game.map_window.alpha_window
+
+        self.x, self.y = x, y
+
     def anime(self):
         pass
 
@@ -21,10 +39,8 @@ class Animation:
 
 class CircularExplosion(Animation):
     def __init__(self, origin):
-        self.game = origin.game
-        self.config = self.game.config.animation.circular_explosion
-        self.x = origin.x
-        self.y = origin.y
+        self.from_origin(origin, origin.game.config.animation.circular_explosion)
+
         self.color = origin.color
         self.expansion_life_time = self.config.expansion_life_time
         self.disappear_life_time = self.config.disappear_life_time
@@ -32,19 +48,15 @@ class CircularExplosion(Animation):
 
         if isinstance(origin, towerClass.HomeTower):
             self.explosion = self.game.complete_destruction
-
             self.size = 2000 * 1 / self.game.zoom
-            self.screen = origin.alpha_screen
+            # self.screen = origin.alpha_screen
         else:
             self.explosion = origin.damaging
             self.size = origin.range
-            self.screen = self.game.map_window.window
 
         self.exploded = False
-        self.type = "CircularExplosion"
 
     def anime(self):
-        self.screen.fill(0)
         self.life_time -= self.game.moving_action
         if self.life_time > self.disappear_life_time:
             advancement = (
@@ -64,7 +76,7 @@ class CircularExplosion(Animation):
 
             advancement = 1 - self.life_time / self.disappear_life_time
             pygame.draw.circle(
-                self.screen,
+                self.alpha_screen,
                 self.color
                 + tuple([int(255 * math_functions.decreasing_cube(advancement))]),
                 (self.game.view_x(self.x), self.game.view_y(self.y)),
@@ -72,12 +84,11 @@ class CircularExplosion(Animation):
             )
         else:
             self.over()
-        self.game.screen.blit(self.screen, (0, 0))
 
 
 class ViewMove(Animation):
     def __init__(self, game, x_end, y_end, zoom_end, speed, tracking=False):
-        self.game = game
+        super().__init__(game, {}, 0, 0)
         self.x_end = x_end
         self.y_end = y_end
         self.zoom_end = zoom_end
@@ -87,14 +98,12 @@ class ViewMove(Animation):
         self.num_frames = 60 // self.speed
         self.c = 0
 
-        self.x, self.y = None, None
         self.x_move = (self.x_end - self.game.view_center_x) / self.num_frames
         self.y_move = (self.y_end - self.game.view_center_y) / self.num_frames
         self.zoom_move = (self.zoom_end / self.game.zoom) ** (1 / self.num_frames)
 
         self.tracking = tracking
         self.game.tracking = False
-        self.type = "ViewMove"
 
     def anime(self):
         if self.c < self.num_frames:
@@ -107,84 +116,62 @@ class ViewMove(Animation):
             self.over()
 
 
-class Particle:
+class Particle(Animation):
     def __init__(self, group, origin):
-        self.game = origin.game
-        self.config = group.config.particle
-        self.x = origin.x
-        self.y = origin.y
+        self.from_origin(origin, group.config.particle)
         self.origin = group
-        self.original_life_time = self.config.get_val("life_time")
-        self.life_time = self.original_life_time
-        self.size = self.config.get_val("size_factor") * origin.size
-        self.color = tuple(
-            min(
-                255,
-                max(
-                    0,
-                    color_comp
-                    + self.config.get_val("color_variation", integer_only=True),
-                ),
-            )
-            for color_comp in origin.color
-        )
-        self.original_speed = self.config.get_val("speed")
-        self.original_alpha = self.config.get_val("alpha")
+        self.size = self.config.size_factor * origin.size
+        self.color = tuple(min(255, max(0, c + int(self.config.color_variation))) for c in origin.color)
+        self.original_speed = self.config.speed
         self.speed = self.original_speed
+        self.speed_decrease = self.config.speed_decrease
+        self.original_alpha = self.config.alpha
         self.alpha = self.original_alpha
+        self.alpha_decrease = self.config.alpha_decrease
+
         theta = random.random() * 2 * pi
         self.x_move = cos(theta)
         self.y_move = sin(theta)
-        self.speed_decrease = self.config.get_val("speed_decrease")
-        self.alpha_decrease = self.config.get_val("alpha_decrease")
-        self.type = "Particle"
 
-    def move(self):
-        self.x += self.speed * self.x_move * self.game.moving_action
-        self.y += self.speed * self.y_move * self.game.moving_action
-        self.speed = self.original_speed * (
-            1 - self.speed_decrease * (1 - self.life_time / self.original_life_time)
-        )
-        self.alpha = self.original_alpha * (
-            1 - self.alpha_decrease * (1 - self.life_time / self.original_life_time)
-        )
+    def anime(self):
         self.life_time -= self.game.moving_action
-
-    def display(self):
-        pygame.draw.circle(
-            self.game.map_window.window,
-            (self.color[0], self.color[1], self.color[2], int(self.alpha)),
-            (self.game.view_x(self.x), self.game.view_y(self.y)),
-            self.size * self.game.zoom,
-        )
-
-    def clean(self):
-        if self.life_time <= 0:
+        if self.life_time >= 0:
+            self.x += self.speed * self.x_move * self.game.moving_action
+            self.y += self.speed * self.y_move * self.game.moving_action
+            self.speed = self.original_speed * (
+                1 - self.speed_decrease * (1 - self.life_time / self.original_life_time)
+            )
+            self.alpha = self.original_alpha * (
+                1 - self.alpha_decrease * (1 - self.life_time / self.original_life_time)
+            )
+            pygame.draw.circle(
+                self.alpha_screen,
+                (self.color[0], self.color[1], self.color[2], int(self.alpha)),
+                (self.game.view_x(self.x), self.game.view_y(self.y)),
+                self.size * self.game.zoom,
+            )
+        else:
             self.origin.particles_bin.add(self)
+
 
 
 class ParticleExplosion(Animation):
     def __init__(self, origin):
-        self.game = origin.game
+        self.from_origin(origin, {})
         if self.game.recognize(origin, "zombie"):
-            self.config = self.game.config.animation.particle_explosion_zombie
+            self.config |= self.game.config.animation.particle_explosion_zombie
         else:
-            self.config = self.game.config.animation.particle_explosion
-        self.particles = set(
-            Particle(self, origin)
-            for _ in range(self.config.get_val("num_particles", integer_only=True))
-        )
+            self.config |= self.game.config.animation.particle_explosion
+
+        self.particles = set(Particle(self, origin) for _ in range(int(self.config.num_particles)))
         self.particles_bin = set()
-        self.type = "ParticleExplosion"
 
     def anime(self):
         if len(self.particles) == 0:
             self.over()
         else:
             for particle in self.particles:
-                particle.move()
-                particle.display()
-                particle.clean()
+                particle.anime()
             self.clean()
 
     def clean(self):
@@ -195,19 +182,14 @@ class ParticleExplosion(Animation):
             self.particles_bin.clear()
 
 
-class TowerBop:
+class TowerBop(Animation):
     id = 0
 
     def __init__(self, origin):
-        self.id += 1
-        self.origin = origin
-        self.game = self.origin.game
-        self.config = self.game.config.animation.tower_bop
-        self.original_life_time = self.config.life_time
-        self.life_time = self.original_life_time
+        self.from_origin(origin, origin.game.config.animation.tower_bop)
+
         self.original_size = self.origin.original_size
         self.size_increase = self.config.size_increase
-        self.type = "TowerBop"
 
     def anime(self):
         self.life_time -= self.game.moving_action
@@ -218,23 +200,17 @@ class TowerBop:
             )
         else:
             self.origin.size = self.original_size
-            self.game.animations_bin.add(self)
+            self.over()
 
 
-class CircularEffect:
+class CircularEffect(Animation):
     def __init__(self, origin, size):
-        self.game = origin.game
+        self.from_origin(origin, origin.game.config.animation.circular_effect)
         self.screen = origin.alpha_screen
-        self.config = self.game.config.animation.circular_effect
-        self.x = origin.x
-        self.y = origin.y
         self.color = origin.color
         self.size = size
         self.original_alpha = self.config.alpha
         self.alpha = self.original_alpha
-        self.original_life_time = self.config.life_time
-        self.life_time = self.original_life_time
-        self.type = "CircularEffect"
 
     def anime(self):
         self.life_time -= self.game.moving_action
